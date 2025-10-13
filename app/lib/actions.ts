5ad8ece21f44d91f7bab29b62ea7be4655eab9a5
punyawatdev <punyawatdev@gmail.com>
2025-10-13 13:55:46 +0700

    fix sql use ssl for online database

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,8 +7,7 @@ import postgres from 'postgres';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-const sql = postgres(process.env.POSTGRES_URL!);
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
   id: z.string(),
@@ -112,7 +111,7 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  // throw new Error('Failed to delete invoice.');
+  throw new Error('Failed to delete invoice.'); // Testing error handling
 
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
